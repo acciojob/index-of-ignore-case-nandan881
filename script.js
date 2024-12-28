751191b168e575cf22e00a1d93a3c5e75c3b7249
Nandan kumar <kumarnandan551@gmail.com>
2024-12-28 19:01:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
-function indexOfIgnoreCase(s1, s2) {
-  // write your code here
+function indexOfIgnoreCase(str, subStr) {
+  // Convert both strings to lowercase to perform case-insensitive comparison
+  const lowerStr = str.toLowerCase();
+  const lowerSubStr = subStr.toLowerCase();
+
+  // Use indexOf to find the first occurrence
+  return lowerStr.indexOf(lowerSubStr);
 }
 
-// Please do not change the code below
-const s1 = prompt("Enter s1:");
-const s2 = prompt("Enter s2:");
-alert(indexOfIgnoreCase(s1, s2));
+// Examples
+console.log(indexOfIgnoreCase("Hello World", "world")); // Output: 6
+console.log(indexOfIgnoreCase("apple", "Ple"));         // Output: 2
+console.log(indexOfIgnoreCase("test", "aaa"));          // Output: -1
